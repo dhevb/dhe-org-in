3300a65d1e666bbfb3e9dfc299161c3fa097a666
unknown <interns.dhe@gmail.com>
2023-12-24 14:11:45 +0530

    text color

--- src/app/component/DirectorMessage.tsx
+++ src/app/component/DirectorMessage.tsx
@@ -1,18 +1,19 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 
 const DirectorMessage: React.FC = () => {
   return (
     <div className="bg-white p-4 items-center">
       <h2 className="text-2xl font-semibold mb-4 text-black">Director Message</h2>
-      <img
+      <Image
   src="/director.JPG"
   alt="Director's Image"
   className="w-80 h-80 md:w-80 md:h-80 lg:w-80 lg:h-80 rounded-full shadow-lg border-2 border-red-600 mx-auto"
  
 />
 
-      <p className = "text-center font-semibold">
+      <p className = "text-center font-semibold text-black">
         <strong>Dr. Thakur SKR</strong>
         <br />
         Your thoughts decide your destiny
