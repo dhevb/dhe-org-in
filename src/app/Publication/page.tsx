7db0aaf35d0fddad43c28afc29b99d3843f3a5e4
unknown <interns.dhe@gmail.com>
2023-12-24 14:25:19 +0530

    text color

--- src/app/Publication/page.tsx
+++ src/app/Publication/page.tsx
@@ -5,38 +5,34 @@ import Publication from "../component/Publication";
 
 
 export default function Home() {
- 
-  const MobileView = () => (
-    <div className="flex flex-col space-y-4 items-center">
-    <Publication/>
-      <div></div>
-      <div className="items-center"></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
-
-  // Components for Desktop View
-  const DesktopView = () => (
-    <div className="flex flex-row space-x-4">
-      <div className="w-1/5"></div>
-      <div className="w-3/5">
-       <Publication/>
-      </div>
-      <div className="w-1/5"></div>
-    </div>
-  );
+    const MobileView = () => (
+        <div className="flex flex-col space-y-4 items-center">
+          <Publication />
+          {/* Remove unnecessary div elements */}
+        </div>
+      );
+      
+      const DesktopView = () => (
+        <div className="flex flex-row space-x-4">
+          <div className="w-1/5"></div>
+          <div className="w-3/5">
+            <Publication />
+          </div>
+          <div className="w-1/5"></div>
+        </div>
+      );
+      
   return (
     <div className="bg-white">
       <CompanyInfo/>
       <Header />
       <div>
         {/* Show Mobile or Desktop View based on screen size */}
-        <div className="sm:hidden">
-          <MobileView />
-        </div>
         <div className="hidden sm:block">
           <DesktopView />
+        </div>
+        <div className="sm:hidden">
+          <MobileView />
         </div>
       </div>
       <BottomView />
