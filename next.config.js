e42c5b7890019b983252027ec4e80518451a54d5
unknown <interns.dhe@gmail.com>
2023-12-25 18:04:02 +0530

    image

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        formats: ['image/avif', 'image/webp', 'image/png', 'image/jpeg'],
-      },
+    
 }
 
 module.exports = nextConfig
