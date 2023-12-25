f91e8005f453de452c04647430bfe16bf9eca510
unknown <interns.dhe@gmail.com>
2023-12-25 17:45:05 +0530

    search

--- src/app/component/StructurePage.tsx
+++ src/app/component/StructurePage.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Structure: React.FC = ()=>{
@@ -5,7 +6,7 @@ const Structure: React.FC = ()=>{
     return(
         <div className="text-center bg-white">
           <h2 className="text-2xl font-semibold pt-4 text-black">Department Structure</h2>
-        <img src="/structure.PNG" alt="Image" className=" mx-auto" />
+        <Image src="/structure.PNG" alt="Image" className=" mx-auto" />
         
       </div>
     )
