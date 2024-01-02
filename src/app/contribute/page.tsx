443332098bcd9bc9b2e561142b6c252ad1cafdc3
unknown <interns.dhe@gmail.com>
2024-01-02 18:01:05 +0530

    benifits

---
+++ src/app/contribute/page.tsx
@@ -0,0 +1,18 @@
+import BottomView from "../component/BottomView";
+import MembershipBenefits from "../component/MembershipBenefits";
+import CompanyInfo from "../component/CompanyInfo";
+import Header from "../component/Header";
+
+
+
+
+export default function Committee() {
+  return (
+    <>
+    <CompanyInfo/>
+    <Header />
+    <MembershipBenefits/>
+     <BottomView />
+  </>
+  )
+}
