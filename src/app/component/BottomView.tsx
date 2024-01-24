613d42af4d9a0673b086cad54330569f6b27e851
unknown <interns.dhe@gmail.com>
2024-01-24 16:16:51 +0530

    icon change

--- src/app/component/BottomView.tsx
+++ src/app/component/BottomView.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter as faXTwitter,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 const BottomView: React.FC = () => {
   return (
@@ -79,9 +86,44 @@ const BottomView: React.FC = () => {
             ></iframe>
           </div>
         </div>
-        <div className="mt-4 text-center">
-          &copy; {new Date().getFullYear()} Department of Holistic Education. All rights reserved.
-        </div>
+       <div className="flex flex-row justify-between">
+  <div className="mt-4 text-white">
+    &copy; {new Date().getFullYear()} Department of Holistic Education. All rights reserved.
+  </div>
+
+  <div className="mt-4 flex items-end space-x-4 bg-white">
+    <a
+      href="https://twitter.com/raseconferences"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon={faXTwitter} size="lg" className="text-white hover:text-blue-500" />
+    </a>
+    <a
+      href="https://www.facebook.com/raseconferences/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon={faFacebook} size="lg" className="text-white hover:text-blue-500" />
+    </a>
+    <a
+      href="https://www.linkedin.com/in/rase-conferences-3a4b71270"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon={faLinkedin} size="lg" className="text-white hover:text-blue-500" />
+    </a>
+    <a
+      href="https://www.instagram.com/shikshamahakumbh/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FontAwesomeIcon icon={faInstagram} size="lg" className="text-white hover:text-blue-500" />
+    </a>
+  </div>
+</div>
+
+
       </div>
     </footer>
   );
