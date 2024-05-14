e6ca6e1e2fa113ec88db694ed0e64e9f925d2684
Yogesh069 <ykj131@gmail.com>
2024-05-14 17:53:21 +0530

    structure made

--- src/app/component/StructurePage.tsx
+++ src/app/component/StructurePage.tsx
@@ -1,15 +1,75 @@
-import Image from "next/image";
 import React from "react";
+import { Tree, TreeNode } from "react-organizational-chart";
+import "tailwindcss/tailwind.css";
 
-const Structure: React.FC = ()=>{
-
-    return(
-        <div className="text-center bg-white">
-          <h2 className="text-2xl font-semibold pt-4 text-primary-color">Department Structure</h2>
-        <Image src="/structure.PNG" alt="Image" className=" mx-auto" height={1000} width={1000} />
-        
-      </div>
-    )
-} 
+const Structure = () => (
+  <div className="flex justify-center p-4">
+    <div className="text-white font-bold text-xl overflow-x-auto">
+      <Tree
+        lineWidth={"2px"}
+        lineColor={"green"}
+        lineBorderRadius={"10px"}
+        label={
+          <div className="text-center p-2">
+           <button className="bg-primary p-2 rounded-lg">
+            Director
+            <br />
+            Department of Holistic Education
+            </button>
+          </div>
+        }
+      >
+        <TreeNode
+          label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Working Committee</button></div>}
+        ></TreeNode>
+        <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cells</button></div>}>
+          <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 1</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 9</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 17</button></div>} />
+            </TreeNode>
+          </TreeNode>
+          <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 1</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 9</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 17</button></div>} />
+            </TreeNode>
+          </TreeNode>
+          <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 1</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 9</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 17</button></div>} />
+            </TreeNode>
+          </TreeNode>
+          <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 1</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 9</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 17</button></div>} />
+            </TreeNode>
+          </TreeNode>
+          <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 1</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 9</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 17</button></div>} />
+            </TreeNode>
+          </TreeNode>
+          <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 1</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 9</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 17</button></div>} />
+            </TreeNode>
+          </TreeNode>
+          <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 1</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 9</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 17</button></div>} />
+            </TreeNode>
+          </TreeNode>
+          <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 1</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 9</button></div>}>
+            <TreeNode label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Cell 17</button></div>} />
+            </TreeNode>
+          </TreeNode>
+        </TreeNode>
+        <TreeNode
+          label={<div className="text-center p-2"><button className="bg-primary p-2 rounded-lg">Advisory Committee</button></div>}
+        ></TreeNode>
+      </Tree>
+    </div>
+  </div>
+);
 
 export default Structure;
