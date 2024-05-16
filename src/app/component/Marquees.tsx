85770daf0ade35b157150aaa76f7601980db5a5f
Yogesh069 <ykj131@gmail.com>
2024-05-16 13:02:58 +0530

    updated info

--- src/app/component/Marquees.tsx
+++ src/app/component/Marquees.tsx
@@ -11,7 +11,6 @@ const Marquees: React.FC = () => {
 
   const marquees: Item[] = [
     { imageUrl: '/new.gif', text: "Dr. Thakur SKR, Sci/Engr-SF at ISRO and Director of the Department of Holistic Education & Vidya Bharti Institute of Training and Research, along with Prof. Rajeev Ahuja, Director of IIT Ropar, had the honor of meeting Smt. Droupadi Murmu, the Hon’ble President of India, regarding the Shiksha Mahakumbh 2024.",    link: 'https://www.youtube.com/watch?v=FFfdSd8_XOw' },
-  
     { imageUrl: '/new.gif', text: "आपके साथ ये ख़बर साँझा करते हुए हर्ष हो रहा है कि DHE की HEI को-ऑर्डिनेशन सेल मार्च 11, 2024 साय 6 बजे INST मोहाली जोकि नैशनल इम्पॉर्टन्स का एक संस्थान है के साथ एक MoU साइन करने जा रही है। आपको बता दें कि DHE के प्रयास से अब तक IIT रोपड, NIT जालंधर, सेंट्रल यूनिवर्सिटी भटिंडा, DAV यूनिवर्सिटी जालंधर, आदि संस्थानों के बाद इस बड़े संस्थान के साथ MoU DHE करने जा रहा है।  आप सभी LMC के सम्मानिय सदस्यों से आग्रह है कि आप सभी इसमें मार्च 11, 2024 साय 6 बजे सम्मिलित हो।", link: 'https://sm24.rase.co.in/' },
   { imageUrl: '/new.gif', text: "Shiksha Mahakumbh - IIT Ropar, 4-6  October, 2024", link: 'https://www.youtube.com/watch?v=FFfdSd8_XOw' },
   { imageUrl: '/new.gif', text:"Shiksha-Kumbh - NIT Srinagar,  April, 2024",link: 'https://sk24.rase.co.in/' },
--- src/app/component/MiddleComponent.tsx
+++ src/app/component/MiddleComponent.tsx
@@ -3,27 +3,43 @@ import React from "react";
 const DepartmentInfo: React.FC = () => {
   return (
     <div className="bg-white  p-8 flex flex-col  items-start ">
-    <h1 className="text-2xl font-semibold mb-4 text-primary-color">
+      <h1 className="text-2xl font-semibold mb-4 text-primary-color">
         Welcome to Department of Holistic Education
       </h1>
+
       <p className="text-justify text-black">
-      The Department of Holistic Education is the brainchild of Dr. Thakur SKR who is a distinguished scientist of ISRO, a dedicated social worker and an accomplished author.
+        The foundation of the Department of Holistic Education (DHE) is rooted
+        in the ideology that education should extend beyond academic
+        institutions. Vidya Bharti, the largest educational NGO globally,
+        established a dedicated think tank Department comprising professionals
+        from diverse fields such as academia, bureaucracy, higher and school
+        education, society, scientists, and other experts like advocates and
+        chartered accountants. DHE was conceptualized in 2021 by visionary ISRO
+        scientist Dr Thakur SKR under the guidance of educationist and social
+        reformer Shri Vijay Kumar Nadda, Organising Secretary, Vidya Bharti-
+        North Zine as a pilot project under Vidya Bharti Punjab. Recognizing its
+        remarkable growth and contributions to education, it was elevated to an
+        independent organization under the auspices of Vidya Bharti Institute of
+        Training and Research Trust.
       </p>
-    <br/>
-   
-    <p className="text-justify text-black">
-    The foundation of DHE is based on the ideology that the level of education should not be gauged by academic scores alone. Rather, in true essence, education should aim at holistic development of children and should be multi-disciplinary focusing on skill development, physical fitness, emotional well being and development of strong ethics in students. Holistic education will transform young minds into not only professionally skilled adults but also responsible individuals who are an asset for the nation.
-      </p>
-      <br/>
-    
-    <p className="text-justify text-black"> 
-    Keeping pace with the technology driven world, there is an imperative need of integration of technology with value-based education. Considering the present lacunae and the dynamically evolving landscape of education, the structure of DHE has been designed to effectively bridge the gaps in present education system to make it more effective. At present, DHE has 25 dedicated cells- IT Cell, Industry Coordination Cell, Learning Management System Cell, CSR Cell, R&D Cell, Entrepreneurship Cell, Foreign Language Cell, Atal Tinkering Labs, Environment Cell, Sports Cell etc catering to various aspects of education system with dedicated co-ordinators to lead these cells. Exhaustive research and development is carried out prior to running pilot projects and implementing them in schools and society.
-    
-      </p>
-      
-    <br/>
-    <p className="text-justify text-black">
-    Since inception, the Department has multiple revolutionary projects- E-cycle, Punjab Super 100, Tredul App, Sarvatr App, Swadeshi bazaar App, TUDU App. Jobs360o App, Surbhi Diye, Surbhi Piggy Bag, Surbhi Flower Pot, etc. to its credit. The department has successfully organised mass outreach events viz Shiksha Mahakumbh at NIT Jalandhar and Shiksha Kumbh at NIT Kurukshetra which were attended by dignitaries and stakeholders from all fields – viz politics, bureaucracy, educational stalwarts, corporate houses, media and speakers from all domains. DHE has also launched its quarterly journal “Viksit India” which offers comprehensive coverage of a multitude of educational aspects, from school education to higher education, language pedagogy to technological advancements, child pedagogy to scientific research and explores the realms of ever-evolving domain of education. With the same vigour and enthusiasm, the Department is gearing up for organisation of Shiksha Kumbh at NIT Srinagar and Shiksha Mahakumbh at IIT Ropar along with launch of various innovative projects- One child one hour work per day, Recycling of cloths, paper & plastic, One school one product, International collaborations, etc in the upcoming months.
+      <br />
+
+      <p className="text-justify text-black">
+        Currently, DHE operates under the trust's umbrella. With 25 dedicated
+        cells led by coordinators, DHE spearheads various initiatives aimed at
+        transforming the education system. These include groundbreaking projects
+        such as E-cycle, Punjab Super 100, Tredul, Sarvatr, Holistic Harbour,
+        PoojaWala, TuDu, Swadeshi Bazaar, Jobs360o. DHE has also revived the
+        glorious saga of having dedicated Kumbh/Mahkumbh for each major issue of
+        society prevailing in ancient Bharat by creating annual large scale
+        events in the form of Shiksha Mahakumbh and Shiksha Kumbh, attracting
+        dignitaries and stakeholders from diverse fields. Additionally, DHE laid
+        foundation of a quarterly journal called "Viksit India," covering a wide
+        range of educational topics to make Bharat a self reliant hub in terms
+        of educational and scientific publication. The Department comprises 310
+        professionals from diverse backgrounds who are volunteering their
+        service to shape the nation through education apart from 4 full-time
+        employees and 20 graduate engineer trainees.
       </p>
     </div>
   );
