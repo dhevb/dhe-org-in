ce17ad77931d45a874fd68baf29f68d2ff457c74
Yogesh069 <ykj131@gmail.com>
2024-05-18 13:15:33 +0530

    a

--- src/app/structure/page.tsx
+++ src/app/structure/page.tsx
@@ -1,11 +1,12 @@
-
 import React from "react";
 import StructurePage from "../component/StructurePage";
 
-const page = () => (
-  <div>
-    <StructurePage />
-  </div>
-);
+const page = () => {
+  return (
+    <div>
+      <StructurePage />
+    </div>
+  );
+};
 
 export default page;
