9a98ba5175d1f5fc77df50f5366e0bcd68a9257f
Yogesh069 <ykj131@gmail.com>
2024-07-03 20:13:14 +0530

    "Removed empty lines in Committee page and updated ComingSoon component with text color changes"

--- src/app/comingsoon/page.tsx
+++ src/app/comingsoon/page.tsx
@@ -3,14 +3,10 @@ import ContactUs from "../component/ComactUs";
 import CommingSoon from "../component/CommingSoon"
 
 
-
-
-
 export default function Committee() {
   return (
     <>
     <CommingSoon/>
-    
   </>
   )
 }
