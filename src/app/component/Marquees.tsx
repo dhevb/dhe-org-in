e8e0ba1df1268445fe32aa0e34e376721a6d908d
Yogesh069 <ykj131@gmail.com>
2024-07-07 09:09:22 +0530

    "Updated Marquees component: replaced old announcements with new ones, added image to each announcement, and refactored HTML structure."

--- src/app/component/Marquees.tsx
+++ src/app/component/Marquees.tsx
@@ -10,23 +10,22 @@ const Marquees: React.FC = () => {
   }
 
   const marquees: Item[] = [
-    { imageUrl: '/new.gif', text: "Dr. Thakur SKR, Sci/Engr-SF at ISRO and Director of the Department of Holistic Education & Vidya Bharti Institute of Training and Research, along with Prof. Rajeev Ahuja, Director of IIT Ropar, had the honor of meeting Smt. Droupadi Murmu, the Hon’ble President of India, regarding the Shiksha Mahakumbh 2024.",    link: 'https://www.youtube.com/watch?v=FFfdSd8_XOw' },
-    { imageUrl: '/new.gif', text: "आपके साथ ये ख़बर साँझा करते हुए हर्ष हो रहा है कि DHE की HEI को-ऑर्डिनेशन सेल मार्च 11, 2024 साय 6 बजे INST मोहाली जोकि नैशनल इम्पॉर्टन्स का एक संस्थान है के साथ एक MoU साइन करने जा रही है। आपको बता दें कि DHE के प्रयास से अब तक IIT रोपड, NIT जालंधर, सेंट्रल यूनिवर्सिटी भटिंडा, DAV यूनिवर्सिटी जालंधर, आदि संस्थानों के बाद इस बड़े संस्थान के साथ MoU DHE करने जा रहा है।  आप सभी LMC के सम्मानिय सदस्यों से आग्रह है कि आप सभी इसमें मार्च 11, 2024 साय 6 बजे सम्मिलित हो।", link: 'https://sm24.rase.co.in/' },
-  { imageUrl: '/new.gif', text: "Shiksha Mahakumbh - IIT Ropar, 4-6  October, 2024", link: 'https://www.youtube.com/watch?v=FFfdSd8_XOw' },
-  { imageUrl: '/new.gif', text:"Shiksha-Kumbh - NIT Srinagar,  April, 2024",link: 'https://sk24.rase.co.in/' },
-  { imageUrl: '/new.gif', text:"Shiksha-Kumbh -  NIT Delhi, June, 2024",link: 'https://sk24.rase.co.in/' },
-  { imageUrl: '/new.gif', text: "Shiksha-Kumbh - NIT Hamirpur,  August,  2024", link: 'https://sk24.rase.co.in/' },
+    { imageUrl: '/new.gif', text: "3rd Edition of RASE Conferences concluded successfully at NIT Srinagar on June 29-30, 2024", link: '' },
+    { imageUrl: '/new.gif', text: "Next RASE Conferences of J&K will be held in IIT Jammu. Stay tuned for more updates.", link: '' },
+    { imageUrl: '/new.gif', text: "4th Edition of RASE Conferences is going to held at IIT Ropar on October 4-6, 2024", link: '' },
   ];
 
   return (
-    <div  className='flex flex-row bg-primary-light w-full'>
-     
-    <div className='text-white bg-primary-color  rounded-sm text-center pe-2 text-xl py-1 ' >
-    Announcement
-    </div>
-      <Marquee  pauseOnHover={true}  pauseOnClick={true}>
+    <div className='flex flex-row bg-primary-light w-full'>
+      <div className='text-white bg-primary-color rounded-sm text-center pe-2 text-xl py-1'>
+        Announcement
+      </div>
+      <Marquee pauseOnHover={true} pauseOnClick={true}>
         {marquees.map((marqueeContent, index) => (
-          <p key={index} className='text-white pe-12'>{marqueeContent.text}</p>
+          <div key={index} className='flex items-center pe-12'>
+            <img src={marqueeContent.imageUrl} alt='Announcement Icon' className='h-10 w-12 me-2' />
+            <p className='text-white'>{marqueeContent.text}</p>
+          </div>
         ))}
       </Marquee>
     </div>
