2afec0ae4241010124917c28cc7391733e8ec5d8
Yogesh069 <ykj131@gmail.com>
2024-09-25 19:02:07 +0530

    "Update Modal component: changed height styling from 'lg:h-2/5' to 'h-auto' in modal content div"

--- src/app/component/Modal.tsx
+++ src/app/component/Modal.tsx
@@ -17,7 +17,7 @@ console.log(pathname);
   return (
     shouldRenderModal && isOpen && (
       <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-white p-2 rounded shadow-lg w-2/3 h-300 lg:w-2/5 lg:h-2/5">
+        <div className="bg-white p-2 rounded shadow-lg w-2/3 h-300 lg:w-2/5 h-auto">
           <button
             className="absolute top-4 right-4 bg-primary text-white px-4 py-2 rounded"
             onClick={onClose}
